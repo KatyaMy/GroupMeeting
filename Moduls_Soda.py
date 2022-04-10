c200fae548541bfcb0b6beb5f15b5944ce9eefc4
KatyaMy <20FamGam20@gmail.com>
2022-04-10 16:40:30 -0400

    HW_Soda and My_Drinks https://smartiqa.ru/python-workbook/class#tasks

---
+++ Moduls_Soda.py
@@ -0,0 +1,13 @@
+from OOP_Soda import Soda
+
+drink_1 = Soda('Lime')
+drink_1.get_lime()
+
+drink_2 = Soda('Strawberry')
+drink_2.get_strawberry()
+
+drink_3 = Soda('Blueberry')
+drink_3.get_blueberry()
+
+drink_4 = Soda(' ')
+drink_4.get_classic()
---
+++ OOP_Soda.py
@@ -0,0 +1,17 @@
+class Soda:
+    def __init__(self, additive):
+        self.additive = additive
+
+    def get_lime(self):
+        print(f'Soda with {self.additive}')
+
+    def get_strawberry(self):
+        print(f'Soda with {self.additive}')
+
+    def get_blueberry(self):
+        print(f'Soda with {self.additive}')
+
+    def get_classic(self):
+        print(f'Regular Soda {self.additive}')
+
+
